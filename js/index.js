dd9dbc463bfcf909f352007bb0489a6b58051641
Emine Shaban <emineshaban@icloud.com>
2024-03-18 15:21:28 +0200

    .

--- js/index.js
+++ js/index.js
@@ -2,27 +2,18 @@ let acc = document.getElementsByClassName("q");
 var speed = "500";
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");  
+        this.classList.toggle("active");
 
         let panel = this.nextElementSibling;
         if (panel.style.display === "block") {
-            panel.style.display = "none"; 
+            panel.classList.toggle("aa");
+
+            panel.style.display = "none";
         } else {
-            panel.style.display = "block"; 
+            this.classList.toggle("aa");
+
+            panel.style.display = "block";
         }
     });
 }
 
-
-// // Accordian FAQ
-// var action = "click";
-// var speed = "500";
-
-// $(document).ready(function () {
-//     $('li.q').on(action, function () {
-//         $(this).next()
-//             .slideToggle(speed)
-//             .siblings('li.a')
-//             .slideUp();
-//     });
-// });
