bb306fe3c561780a81c4f6629771a65b9730b24d
Emine Shaban <emineshaban@icloud.com>
2024-03-18 14:45:35 +0200

    .

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@ let acc = document.getElementsByClassName("q");
 var speed = "500";
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
+        this.classList.toggle("active");  
+
         let panel = this.nextElementSibling;
         if (panel.style.display === "block") {
             panel.style.display = "none"; 
         } else {
-            panel.style.display = "block";
-
+            panel.style.display = "block"; 
         }
     });
 }
