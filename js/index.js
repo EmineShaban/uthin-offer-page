51384b0da58e9590720eb5e7db5cc220477c3584
Emine Shaban <emineshaban@icloud.com>
2024-03-19 07:56:04 +0200

    .

--- js/index.js
+++ js/index.js
@@ -17,3 +17,25 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 
+
+
+ 
+// var acc = document.getElementsByClassName("q");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//      panel.style.display = "none";
+
+//     } else {
+//       panel.style.maxHeight = 95 + "px";
+//   panel.style.display = "block";
+
+//     }
+//   });
+// }
+ 
